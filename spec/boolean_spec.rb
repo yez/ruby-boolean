0239649b3780fa66789a404ce6f0d2ea80929d7b
Jake Yesbeck <jake@apartmentlist.com>
2015-04-27 19:45:55 -0700

    Added spec to prove that boolean does exactly what the README says

---
+++ spec/boolean_spec.rb
@@ -0,0 +1,21 @@
+require_relative '../lib/boolean'
+
+describe Boolean do
+  context 'TrueClass' do
+    specify do
+      expect(true.is_a?(Boolean)).to eq(true)
+    end
+  end
+
+  context 'FalseClass' do
+    specify do
+      expect(false.is_a?(Boolean)).to eq(true)
+    end
+  end
+
+  context 'NilClass' do
+    specify do
+      expect(nil.is_a?(Boolean)).to eq(false)
+    end
+  end
+end
