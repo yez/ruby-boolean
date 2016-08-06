21bd86e324ac9557f6036e5de2cd2e78048e39db
charlvj <charlvj@gmail.com>
2016-08-05 22:22:31 -0400

    Added a map method to TrueClass and FalseClass

--- lib/boolean/boolean.rb
+++ lib/boolean/boolean.rb
@@ -1,3 +1,15 @@
 module Boolean; end
-class TrueClass; include Boolean; end
-class FalseClass; include Boolean; end
+class TrueClass
+  include Boolean
+
+  def map(true_value, false_value)
+    true_value
+  end
+end
+class FalseClass
+  include Boolean
+
+  def map(true_value, false_value)
+    false_value
+  end
+end
--- spec/boolean_spec.rb
+++ spec/boolean_spec.rb
@@ -5,11 +5,19 @@ describe Boolean do
     specify do
       expect(true.is_a?(Boolean)).to eq(true)
     end
+
+    specify do
+      expect(true.map(:yes, :no)).to eq(:yes)
+    end
   end
 
   context 'FalseClass' do
     specify do
       expect(false.is_a?(Boolean)).to eq(true)
+    end
+
+    specify do
+      expect(false.map(:yes, :no)).to eq(:no)
     end
   end
 
